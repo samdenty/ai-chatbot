e23fa623f152f342e6e103cea81eb69adc1b2dc5
Sam Denty <sam@samdenty.com>
2025-02-20 18:24:04 +0000

    feat: wire-up to react-ai-flow

--- components/code-block.tsx
+++ components/code-block.tsx
@@ -5,6 +5,7 @@ import { CodeIcon, LoaderIcon, PlayIcon, PythonIcon } from './icons';
 import { Button } from './ui/button';
 import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip';
 import { cn } from '@/lib/utils';
+import { StaggeredText } from 'react-ai-flow';
 
 interface CodeBlockProps {
   node: any;
@@ -35,13 +36,19 @@ export function CodeBlock({
             {...props}
             className={`text-sm w-full overflow-x-auto dark:bg-zinc-900 p-4 border border-zinc-200 dark:border-zinc-700 rounded-xl dark:text-zinc-50 text-zinc-900`}
           >
-            <code className="whitespace-pre-wrap break-words">{children}</code>
+            <StaggeredText>
+              <code className="whitespace-pre-wrap break-words">
+                {children}
+              </code>
+            </StaggeredText>
           </pre>
         )}
 
         {tab === 'run' && output && (
           <div className="text-sm w-full overflow-x-auto bg-zinc-800 dark:bg-zinc-900 p-4 border border-zinc-200 dark:border-zinc-700 border-t-0 rounded-b-xl text-zinc-50">
-            <code>{output}</code>
+            <StaggeredText>
+              <code>{output}</code>
+            </StaggeredText>
           </div>
         )}
       </div>
--- components/markdown.tsx
+++ components/markdown.tsx
@@ -3,11 +3,12 @@ import React, { memo, useMemo, useState } from 'react';
 import ReactMarkdown, { type Components } from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { CodeBlock } from './code-block';
+import { StaggeredText, StaggerProvider } from 'react-ai-flow';
 
 const components: Partial<Components> = {
   // @ts-expect-error
   code: CodeBlock,
-  pre: ({ children }) => <>{children}</>,
+  pre: ({ children }) => <StaggeredText>{children}</StaggeredText>,
   ol: ({ node, children, ...props }) => {
     return (
       <ol className="list-decimal list-outside ml-4" {...props}>
@@ -17,9 +18,11 @@ const components: Partial<Components> = {
   },
   li: ({ node, children, ...props }) => {
     return (
-      <li className="py-1" {...props}>
-        {children}
-      </li>
+      <StaggeredText>
+        <li className="py-1" {...props}>
+          {children}
+        </li>
+      </StaggeredText>
     );
   },
   ul: ({ node, children, ...props }) => {
@@ -32,7 +35,7 @@ const components: Partial<Components> = {
   strong: ({ node, children, ...props }) => {
     return (
       <span className="font-semibold" {...props}>
-        {children}
+        <StaggeredText>{children}</StaggeredText>
       </span>
     );
   },
@@ -45,50 +48,57 @@ const components: Partial<Components> = {
         rel="noreferrer"
         {...props}
       >
-        {children}
+        <StaggeredText>{children}</StaggeredText>
       </Link>
     );
   },
   h1: ({ node, children, ...props }) => {
     return (
       <h1 className="text-3xl font-semibold mt-6 mb-2" {...props}>
-        {children}
+        <StaggeredText>{children}</StaggeredText>
       </h1>
     );
   },
   h2: ({ node, children, ...props }) => {
     return (
       <h2 className="text-2xl font-semibold mt-6 mb-2" {...props}>
-        {children}
+        <StaggeredText>{children}</StaggeredText>
       </h2>
     );
   },
   h3: ({ node, children, ...props }) => {
     return (
       <h3 className="text-xl font-semibold mt-6 mb-2" {...props}>
-        {children}
+        <StaggeredText>{children}</StaggeredText>
       </h3>
     );
   },
   h4: ({ node, children, ...props }) => {
     return (
       <h4 className="text-lg font-semibold mt-6 mb-2" {...props}>
-        {children}
+        <StaggeredText>{children}</StaggeredText>
       </h4>
     );
   },
   h5: ({ node, children, ...props }) => {
     return (
       <h5 className="text-base font-semibold mt-6 mb-2" {...props}>
-        {children}
+        <StaggeredText>{children}</StaggeredText>
       </h5>
     );
   },
   h6: ({ node, children, ...props }) => {
     return (
       <h6 className="text-sm font-semibold mt-6 mb-2" {...props}>
-        {children}
+        <StaggeredText>{children}</StaggeredText>
       </h6>
+    );
+  },
+  p: ({ node, children, ...props }) => {
+    return (
+      <p {...props}>
+        <StaggeredText>{children}</StaggeredText>
+      </p>
     );
   },
 };
@@ -97,9 +107,29 @@ const remarkPlugins = [remarkGfm];
 
 const NonMemoizedMarkdown = ({ children }: { children: string }) => {
   return (
-    <ReactMarkdown remarkPlugins={remarkPlugins} components={components}>
-      {children}
-    </ReactMarkdown>
+    <StaggerProvider
+      // disabled
+      // visualDebug
+      maxFps={60}
+      streaming={false}
+      splitter="word"
+      delayTrailing
+      animation="bounce-in"
+      gradientWidth="300px"
+      stagger={20}
+      duration={(element) => {
+        // if (element.text.innerText.includes('This is an inline code ')) {
+        //   return 10000;
+        // }
+        return 500;
+      }}
+
+      // stagger={(element, previousElement) => (previousElement?.duration ?? 0) * 0.4}
+    >
+      <ReactMarkdown remarkPlugins={remarkPlugins} components={components}>
+        {children}
+      </ReactMarkdown>
+    </StaggerProvider>
   );
 };
 
